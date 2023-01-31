7b9bc5a918500eb85efc621f504867de90086530
Christian Mazzeo <chris.mazzeo@icloud.com>
2023-01-31 15:54:05 -0300

    add initials files

---
+++ App.tsx
@@ -0,0 +1,161 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useEffect, type PropsWithChildren} from 'react';
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section: React.FC<
+  PropsWithChildren<{
+    title: string;
+  }>
+> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  useEffect(() => {
+    (async () => {
+       await checkPreviousSessionCrash();
+    })();
+
+  }, [])
+
+  async function checkPreviousSessionCrash() {
+    console.log("App has creashed")
+    const didCrash = await Crashes.hasCrashedInLastSession();
+    if (didCrash){
+      const report = await Crashes.lastSessionCrashReport();
+      console.log(JSON.stringify(report,null,2));
+
+  Alert.alert('My app has crashed"', JSON.stringify(report,null,2), [
+    {
+      text: 'Cancel',
+      onPress: () => console.log('Cancel Pressed'),
+      style: 'cancel',
+    },
+    {text: 'OK', onPress: () => console.log('OK Pressed')},
+  ]);
+    }
+    
+  }
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+            
+          <Button title='Press to crash' onPress={()=>{throw new Error ('Testing Crash')}}/>
+          <Button title='Press to crash ' onPress={()=>{
+            Crashes.generateTestCrash();
+            }}/>
+           <Button title='Press to Track an event ' onPress={()=>{
+            Analytics.trackEvent("My custom event");
+            }}/>
+            <Button title='Press to Track an event with properties ' onPress={()=>{
+              Analytics.trackEvent("My custom event",{MyKey :"Value", MyKey2:"Value2" });
+            }}/>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ReactNativeCICD
+<!--- 
+[![Build status](https://build.appcenter.ms/v0.1/apps/c1049a87-b866-48c8-868b-3ed00cbbe73d/branches/dev/badge)](https://appcenter.ms)
+-->
+[![Build status](https://build.appcenter.ms/v0.1/apps/b1759048-2b85-4de9-92c8-f8ef54e151f2/branches/dev/badge)](https://appcenter.ms)
+
+
+Android 
+1. Firma Release
+	1. Crear una carpeta fuera del proyecto llamada <MyProject>_Certificate
+	2. Generar en esa carpeta el Keystore desde Android Studio --> Build --> Generate Signed Bundle/APK 
+	3. Actualizar keystore.properties con los valores
+	3. Remover keystore.properties del repositorio (tiene que quedar solo local) y agregarlo al .gitignore
